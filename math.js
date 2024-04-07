f34b02dcaee348f8b0e241a0839615e99e95b91a
ModabbirHossainAkanda <modabbirhossain041@gmail.com>
2024-04-07 15:41:58 +0300

    Math

---
+++ math.js
@@ -0,0 +1,41 @@
+const result = Math.pow(2, 3);
+// console.log(result);
+
+const num1 = 75;
+const num2 = 45;
+
+const gap = num1 - num2;
+// console.log(gap);
+
+if(gap < 5){
+    // console.log('you guys can be friends')
+}
+else{
+    // console.log('you guys stay apart');
+}
+
+
+// ..........................
+// ...........................
+// ...........................
+const number = 2.49198;
+const fullNumber = Math.round(number);
+// console.log(fullNumber);
+const result2 = Math.ceil(2.000000001);
+const result3 = Math.floor(23.45352);
+// console.log(result2);
+// console.log(result3);
+
+
+// ..................
+// ..................
+// random
+
+// console.log(Math.random());
+const random = Math.round(Math.random()*100);
+console.log(random);
+
+for(let i=0; i < 20; i++){
+    const random = Math.round(Math.random()*6);
+    console.log(random);
+}
