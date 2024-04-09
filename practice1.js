45bfbfc8eb17015215198d95b8a3b996a5264bea
ModabbirHossainAkanda <modabbirhossain041@gmail.com>
2024-04-09 19:02:18 +0300

    practice1

---
+++ practice1.js
@@ -0,0 +1,13 @@
+const jamal = 30;
+const kamal = 20;
+const himu = 90;
+
+if(jamal > kamal && jamal > himu){
+    console.log('Jamal will win the lattery');
+}
+else if(kamal > jamal && kamal > himu){
+    console.log('Kamal will win the lottery');
+}
+else{
+    console.log('Himu will win the lottery');
+}
