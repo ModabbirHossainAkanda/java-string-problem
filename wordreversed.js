74ced29694bf14784762d7d59d22a6440612e874
ModabbirHossainAkanda <modabbirhossain041@gmail.com>
2024-04-10 16:52:40 +0300

    fibo add

---
+++ wordreversed.js
@@ -0,0 +1,24 @@
+// function reverseWords(str){
+//     const words = str.split(' ');
+//     console.log(words);
+// }
+
+
+// const myString = 'I am a good boy';
+// reverseWords(myString);
+
+
+// reversed way.........
+
+function reverseWords(str){
+    const words = str.split(' ');
+    const result = [];
+    for(let i = words.length - 1; i >=0; i--){
+        const element = words[i];
+        result.push(element);
+    }
+    const reversed = result.join(' ');
+    return reversed;
+}
+const myString = 'I am a good boy';
+reverseWords(myString);
